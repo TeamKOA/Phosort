0930d8a1d2d77af0e4c30e4408a3e9d6abe711fc
TeamKOA <34453516+TeamKOA@users.noreply.github.com>
2019-08-13 10:35:39 +0200

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Phosort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fdlg_open = new System.Windows.Forms.FolderBrowserDialog();
+            this.fdlg_save = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fdlg_open
+            // 
+            this.fdlg_open.Description = "Select the source folder";
+            // 
+            // fdlg_save
+            // 
+            this.fdlg_save.Description = "Select the destination folder";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(788, 489);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 514);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog fdlg_open;
+        private System.Windows.Forms.FolderBrowserDialog fdlg_save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phosort
+{
+    public partial class Form1 : Form
+    {
+        bool ratio = false;
+
+        string openPath;
+        string savePath;
+
+        string[] files;
+        int currentItem = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyEvent);
+            this.ResizeEnd += new EventHandler(Form1_Resized);
+        }
+
+        #region KeyHandling
+        private void Form1_KeyEvent(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    Next();
+                    break;
+                case Keys.Left:
+                    Previous();
+                    break;
+                case Keys.Up:
+                    Save();
+                    break;
+                case Keys.A:
+                    ratio = !ratio;
+                    break;
+            }
+        }
+
+        void Next()
+        {
+            currentItem = currentItem++ > files.Length - 1 ? 0 : currentItem++;
+            LoadPicture();
+        }
+
+        void Previous()
+        {
+            currentItem = currentItem-- < 0 ? files.Length - 1 : currentItem--;
+            LoadPicture();
+        }
+
+        void Save()
+        {
+            File.Copy(files[currentItem], files[currentItem].Replace(openPath, savePath));
+        }
+        #endregion
+
+        private void Form1_Resized(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size((int)((this.Size.Height - 75 )* (ratio ? (16F / 9F) : 1.5F)), this.Size.Height - 75);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult openRes = fdlg_open.ShowDialog();
+            DialogResult saveRes = fdlg_save.ShowDialog();
+            if (openRes != DialogResult.OK || saveRes != DialogResult.OK)
+            {
+                //Error handling lol
+            }
+            openPath = fdlg_open.SelectedPath;
+            savePath = fdlg_save.SelectedPath;
+
+            files = Directory.GetFiles(openPath);
+
+            List<string> filteredFiles = new List<string>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].ToLower().EndsWith(".jpg"))
+                    filteredFiles.Add(files[i]);
+            }
+            files = filteredFiles.ToArray<string>();
+
+            LoadPicture();
+        }
+
+        private void LoadPicture()
+        {
+            pictureBox1.ImageLocation = files[currentItem];
+            pictureBox1.Load();
+        }
+    }
+}
